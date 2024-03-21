f1655fc6a47c97b0ed392d14b6ea24d5e84250fe
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-21 20:56:01 -0300

    feat: solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let temperatureToCelsius = (temperature - 32) * (5 / 9);
+  temperatureToCelsius = Math.round(temperatureToCelsius * 10) / 10;
+
+  return temperatureToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let temperatureToFahrenheit = temperature * (9 / 5) + 32;
+  temperatureToFahrenheit = Math.round(temperatureToFahrenheit * 10) / 10;
+
+  return temperatureToFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
