14023aee9f586839040849e7f1c4e7dd9609d729
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-21 18:39:53 -0300

    feat: solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringToArray = string.split("");
+  let reversedStringToArray = stringToArray.reverse();
+  let reversedString = reversedStringToArray.join("");
 
+  return reversedString;
 };
 
 // Do not edit below this line
