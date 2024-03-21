40d4378815e2a9d042ce97edd2a984b7c74d8835
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-21 20:29:41 -0300

    feat: solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear;
 
+  if (
+    (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) ||
+    (year % 4 === 0 && year % 100 !== 0)
+  ) {
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
+
+  return isLeapYear;
 };
 
 // Do not edit below this line
