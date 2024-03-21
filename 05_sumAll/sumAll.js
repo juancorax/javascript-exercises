ab5cde14cd6c25104b77a9800ca0d708bbbc2e43
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-21 20:10:50 -0300

    feat: solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstInteger, secondInteger) {
+  if (
+    firstInteger < 0 ||
+    secondInteger < 0 ||
+    typeof firstInteger !== "number" ||
+    typeof secondInteger !== "number"
+  ) {
+    return "ERROR";
+  } else if (firstInteger > secondInteger) {
+    let firstIntegerBackup = firstInteger;
+    firstInteger = secondInteger;
+    secondInteger = firstIntegerBackup;
+  }
 
+  let finalSum = 0;
+
+  for (let i = firstInteger; i <= secondInteger; i++) {
+    finalSum += i;
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
