3b2c1a991e89e655847ae627e9876aec71bd1c78
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-21 19:49:50 -0300

    feat: solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementsToRemove) {
+  for (let i = 0; i < elementsToRemove.length; i++) {
+    let sameValueAmount = array.filter(
+      (element) => element === elementsToRemove[i],
+    ).length;
 
+    for (let j = 1; j <= sameValueAmount; j++) {
+      array.splice(array.indexOf(elementsToRemove[i]), 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
