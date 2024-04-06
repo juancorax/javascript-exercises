552f2f7d0ca51e282e77acf1765e82d272d753ac
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-05 23:40:01 -0300

    feat: solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((oldest, current) => {
+    let currentAge;
+    let oldestAge;
 
+    if ("yearOfDeath" in current) {
+      currentAge = current.yearOfDeath - current.yearOfBirth;
+    } else {
+      const currentYear = new Date().getFullYear();
+      currentAge = currentYear - current.yearOfBirth;
+    }
+
+    if ("yearOfDeath" in oldest) {
+      oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    } else {
+      const currentYear = new Date().getFullYear();
+      oldestAge = currentYear - oldest.yearOfBirth;
+    }
+
+    if (currentAge > oldestAge) {
+      return current;
+    }
+    return oldest;
+  });
 };
 
 // Do not edit below this line
