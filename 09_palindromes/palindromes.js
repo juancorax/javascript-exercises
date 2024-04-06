d2204f74f7b3c674d9303679c13ce8f532295955
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-05 21:31:38 -0300

    feat: solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const arrayFromString = string.toLowerCase().split("");
 
+  const charactersToExclude = ["!", " ", ",", "."];
+
+  const filteredArray = arrayFromString.filter((item) => {
+    for (let character of charactersToExclude) {
+      if (item === character) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  if (filteredArray.join("") === filteredArray.reverse().join("")) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
