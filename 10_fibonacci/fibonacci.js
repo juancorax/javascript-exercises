e5f8662f6895716c12e80e7bec8d4f0870338c47
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-05 22:49:29 -0300

    feat: solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (fibonacciMember) {
+  fibonacciMember = Number(fibonacciMember);
 
+  if (fibonacciMember === 0) {
+    return 0;
+  } else if (fibonacciMember < 0) {
+    return "OOPS";
+  }
+
+  let precedingNumber1 = 1;
+  let precedingNumber2 = 0;
+
+  for (let i = 2; i <= fibonacciMember; i++) {
+    let currentNumber = precedingNumber1 + precedingNumber2;
+    precedingNumber2 = precedingNumber1;
+    precedingNumber1 = currentNumber;
+  }
+
+  return precedingNumber1;
 };
 
 // Do not edit below this line
